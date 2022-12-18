f32b793a477b9986314efdd242785e96f2db9093
wangjun <wangjun1@zto.com>
2022-12-18 17:16:41 +0800

    feat: 完成裁切

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import drag from "v-drag";
 
-import "./assets/main.css";
+// import "./assets/main.css";
 
 const app = createApp(App);
 
+app.use(drag);
 app.use(router);
 
 app.mount("#app");
--- src/package/j-btn/index.vue
+++ src/package/j-btn/index.vue
@@ -1,19 +1,92 @@
 <template>
-  <i class="iconfont icon-xiangji1fill"></i>
+  <i
+    @click="onClick"
+    v-if="screenCaptureBtnVisible"
+    class="iconfont icon-xiangji1fill screen-capture-btn"
+  ></i>
+  <ul class="ul-ui">
+    <li v-for="item in [113, 2, 3, 4, 5, 6, 7, 8, 9]" :key="item">
+      {{ item }}
+    </li>
+  </ul>
 </template>
 <script>
+import html2canvas from "html2canvas";
 export default {
   name: "JBtn",
+  data() {
+    return {
+      screenCaptureBtnVisible: true,
+    };
+  },
   methods: {
+    /**
+     * 裁切方法
+     */
+    screenCaptureHandler() {
+      const configs = {
+        useCORS: true,
+        allowTaint: true,
+      };
+      return html2canvas(document.body, configs).then((canvas) => {
+        // canvas为转换后的Canvas对象
+        let tempImg = new Image();
+        tempImg.src = canvas.toDataURL(); // 导出图片
+        // 牵扯跨域
+        this.downloadNetMethod(tempImg.src, "海报");
+      });
+    },
+    /**
+     * 下载裁切
+     */
+    downloadNetMethod(src, name) {
+      var eleLink = document.createElement("a");
+      eleLink.href = src; // 转换后的图片地址
+      eleLink.download = name;
+      document.body.appendChild(eleLink);
+      // 触发点击
+      eleLink.click();
+      // 然后移除
+      document.body.removeChild(eleLink);
+    },
+
     onClick() {
-      console.log(111);
+      // 隐藏裁切按钮
+      this.screenCaptureBtnVisible = false;
+      // 等待数据相应后，在执行裁切
+      this.$nextTick(async () => {
+        // 异步裁切
+        await this.screenCaptureHandler();
+        // 恢复按钮显示
+        this.screenCaptureBtnVisible = true;
+      });
     },
   },
 };
 </script>
-<style>
+<style scoped lang="scss">
+@import "@/assets/main.css";
 .btn {
   color: red;
   font-size: 50px;
+}
+.ul-ui {
+  li {
+    padding: 50px;
+    margin-bottom: 30px;
+    background: yellow;
+  }
+}
+.screen-capture-btn {
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  border: 1px solid rgba(0, 128, 0, 0.4);
+  z-index: 999;
+  padding: 10px;
+  border-radius: 50%;
+  font-size: 20px;
+  cursor: pointer;
+  color: green;
 }
 </style>
